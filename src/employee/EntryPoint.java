0095c9cb17047ac1aa3d2c1961a6fabd17e570d2
ABHISHEK PRALHAD BHALERAO <59381252+ABAppDev@users.noreply.github.com>
2020-11-03 10:23:04 +0530

    Q6

--- src/employee/EntryPoint.java
+++ src/employee/EntryPoint.java
@@ -2,18 +2,16 @@ package employee;
 
 public class EntryPoint {
 
-    
-    public static void main(String[] args) 
-	{
-		AreaSalesManager a1 = new AreaSalesManager();
-		a1.display1();
-		System.out.println("Employee Yearly salary is : "+a1.calSal1());
-		a1.display2();
-		System.out.println("Managers basic salary is : "+a1.manSal);
-		System.out.println("Managers salary + Bonus is : "+a1.calSal2());
-		a1.display3();
-		System.out.println("AreaSalesManager salary with other Allowance is : "+a1.calSal3());
-		
-	}
-    
+    public static void main(String[] args) {
+	AreaSalesManager a1 = new AreaSalesManager();
+	a1.display1();
+	System.out.println("\nEmployee Yearly salary is : " + a1.calSal1());
+	a1.display2();
+	System.out.println("\nManagers basic salary is : " + a1.manSal);
+	System.out.println("\nManagers salary + Bonus is : " + a1.calSal2());
+	a1.display3();
+	System.out.println("\nAreaSalesManager salary with other Allowance is : " + a1.calSal3());
+
+    }
+
 }
